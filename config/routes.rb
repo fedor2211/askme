df07984b7f29f1f6d3cb0d71025f38ac6c4d1621
Gwarek2 <fedorsah@gmail.com>
2022-12-14 13:58:34 +0300

    Added users to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
+
+  resources :users, only: %i[new create]
+
+  resource :session, only: %i[new create destroy]
   
   resources :questions do
     put 'hide', on: :member
