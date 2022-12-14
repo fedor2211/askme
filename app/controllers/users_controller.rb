174ef89b5c3ca01ca36d27c65e708bc4bedd4910
Gwarek2 <fedorsah@gmail.com>
2022-12-14 13:59:07 +0300

    Added authomatic user login after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:name, :nickname, :email, :password)
     @user = User.create(user_params)
+    session[:user_id] = @user.id
 
     redirect_to root_path, notice: 'Вы успешно зарегистрировались'
   end
