f4e52e6c19ca4c80451a99e091153d2f6a030964
Gwarek2 <fedorsah@gmail.com>
2022-12-16 15:22:26 +0300

    Added validations for user and question models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A\w+@\w+\.[A-Za-z]+\z/ }
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A\w+\z/ }
   has_secure_password
 
   before_save :downcase_nickname
