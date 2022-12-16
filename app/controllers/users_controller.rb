ba184b8b4c65a376851e20edc4b307a5ffe897a9
Gwarek2 <fedorsah@gmail.com>
2022-12-16 14:45:07 +0300

    Added password confirmation and user data update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,42 @@
 class UsersController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
-    @user = User.create(user_params)
-    session[:user_id] = @user.id
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
+    @user = User.new(user_params)
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
+      render :new
+    end
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Данные пользователя обновлены'
+    else
+      flash.now[:alert] = 'При попытке сохранить пользователя возникли ошибки'
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params(:id))
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'Пользователь удалён'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  before_save :downcase_nickname
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
