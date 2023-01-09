bc5ea0b425f1163bf9dd821ce00a63756a587721
fedor <fedorsah@gmail.com>
2023-01-09 21:43:12 +0500

    Improve and refactor question view

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,4 +2,8 @@ module QuestionsHelper
   def hashtag_link(hashtag)
     link_to "##{hashtag.name}", root_path(tag: hashtag.name)
   end
+
+  def question_link(question)
+    link_to "Вопрос #{question.id}", question_path(question)
+  end
 end
