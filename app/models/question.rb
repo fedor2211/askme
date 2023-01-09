6b56d7d0ce459fb2eca372375c3529161d2de188
fedor <fedorsah@gmail.com>
2023-01-09 21:41:18 +0500

    Add reference removal for question unpresented hashtags

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,7 +6,7 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: "User", optional: true
   has_many :hashtag_questions, dependent: :destroy
   has_many :hashtags, through: :hashtag_questions
-  after_save :save_hashtags
+  after_save :update_hashtags
 
   def hidden?
     hidden == true
@@ -14,13 +14,22 @@ class Question < ApplicationRecord
 
   private
 
-  def save_hashtags
+  def update_hashtags
+    old_hashtags = hashtags.map(&:name)
     new_hashtags = HashtagParser.parse(body)
     new_hashtags += HashtagParser.parse(answer) if answer.present?
     new_hashtags.uniq!
-    new_hashtags.each do |hashtag_text|
-      hashtag = Hashtag.find_or_create_by(name: hashtag_text.downcase)
-      HashtagQuestion.create(question: Question.find(id), hashtag: hashtag)
+    new_hashtags.each do |hashtag_name|
+      hashtag = Hashtag.find_or_create_by(name: hashtag_name.downcase)
+      HashtagQuestion.create(question: self, hashtag: hashtag)
+    end
+    remove_unpresented_hashtags(old_hashtags - new_hashtags)
+  end
+
+  def remove_unpresented_hashtags(hashtags_to_remove)
+    hashtags_to_remove.each do |hashtag_name|
+      hashtag = Hashtag.find_by(name: hashtag_name)
+      HashtagQuestion.destroy_by(question: self, hashtag: hashtag)
     end
   end
 end
