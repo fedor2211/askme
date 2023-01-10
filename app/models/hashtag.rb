8fe54fd787b92c365a05883a9e84bef7f0a81e3a
fedor <fedorsah@gmail.com>
2023-01-10 15:14:38 +0500

    Replace empty tags removal with hiding in views

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,10 +1,14 @@
 class Hashtag < ApplicationRecord
-  has_many :hashtag_questions
+  has_many :hashtag_questions, dependent: :destroy
   has_many :questions, through: :hashtag_questions
 
   before_validation :downcase_name
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
+
+  def questions?
+    questions.empty?
+  end
 
   private
 
--- app/models/hashtag_question.rb
+++ app/models/hashtag_question.rb
@@ -3,11 +3,4 @@ class HashtagQuestion < ApplicationRecord
   belongs_to :hashtag
 
   validates :question, uniqueness: { scope: :hashtag_id }
-  after_destroy :destroy_empty_hashtag
-
-  private
-
-  def destroy_empty_hashtag
-    hashtag.destroy if hashtag.questions.empty?
-  end
 end
